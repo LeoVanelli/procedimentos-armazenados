fc132befe239181de08f032eab16744af7119eb3
LeoVanelli <55560279+LeoVanelli@users.noreply.github.com>
2020-10-04 01:29:26 -0300

    Update t_nova_conta.sql
    Objetivo: Altere a trigger t_nova_conta, de forma a adicionar a dtabertura_conta como a data e hora do de sistema.

--- triggers/t_nova_conta.sql
+++ triggers/t_nova_conta.sql
@@ -3,8 +3,9 @@ DROP TRIGGER IF EXISTS t_nova_conta$$
 CREATE TRIGGER t_nova_conta BEFORE INSERT ON conta
 FOR EACH ROW
 BEGIN
-   IF (NEW.saldo < 0) THEN
-      SET NEW.saldo = 0;
-   END IF;
+	IF (NEW.saldo < 0) THEN
+		SET NEW.saldo = 0;
+	END IF;
+    SET NEW.dtabertura_conta = CURRENT_TIMESTAMP ;
 END;
 $$
